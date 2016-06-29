f34b7f5c1c9909bd47d5992119b4bfd2771c2443
ags2121 <alexsilva88@gmail.com>
2016-06-28 21:47:06 -0400

    fix js bugs

--- js/main.js
+++ js/main.js
@@ -1,162 +1,31 @@
+// GLOBAL VARS
 var 
-  audioPlayer = $('audio')[0],
-  audioSource = $('audio').find('source'),
   servicesURL = 'http://www.publicradioservices.info',
   // servicesURL = 'http://localhost:3000',
-  newsModule = $('#newscasts');
+  audioPlayer = $('audio')[0],
+  audioSource = $('audio').find('source');
 
-// load default audio
+// NEWSCASTS
+var newsModule = $('#newscasts');
+
+// load BBC Headlines as default audio
 loadAudio(newsModule.find('.bbc-headlines').data('url'));
-// set default audio last update time
-newsModule.find('.bbc-headlines .last-update-time').text(formatDateTime(getBBCHeadlinesLastUpdate()), true)
+
+// set BBC Headlines last update time
+newsModule.find('.bbc-headlines .last-update-time').text(formatDateTime(getBBCHeadlinesLastUpdate()), true);
 newsModule.find('.bbc-headlines').removeClass('spin');
 
-// "loading" messaging
-setInterval(function() { $('.loading .text').toggleClass('black') }, 400); // 0.5 second     
-audioPlayer.oncanplay = function () {
-  $('.loading').hide();
-}
+function getBBCHeadlinesLastUpdate () {
+  var 
+    minsToChangeTo = 35,
+    now = new Date(),
+    nowMins = now.getMinutes();
 
-// Internet Radio
-function addHtmlToNode(node, data) {
-  var totalHtml = '';
-  data.forEach(function ({description, donateurl, streamurl, title, url}) { 
-    var html = 
-    '<li data-url="' + streamurl +'">' +
-      '<h5 class="station-title">' + title + '</h5>' +
-      '<div class="description">' + description + '</div>' +
-      '<div class="internet-links">' +      
-          '<a href=' + url + ' target="_blank">Website</a> | ' + 
-          '<a href=' + donateurl + ' target="_blank">Donate</a>' +
-      '</div>' +
-    '</li>';
-    totalHtml += html;
-  });
-  $(node).html(totalHtml);
-}
+  now.setMinutes(minsToChangeTo);
+  var changeToLastHour = (nowMins - minsToChangeTo < 0);
+  if (changeToLastHour) { now.setHours(now.getHours()-1); }
 
-var internetRadioColumns = $('.internet-ul');
-addHtmlToNode(internetRadioColumns[0], streams.slice(0, 5));
-addHtmlToNode(internetRadioColumns[1], streams.slice(5, 10));
-addHtmlToNode(internetRadioColumns[2], streams.slice(10, 15));
-
-$('.news-ul li, .internet-ul li').on('click', function (e) { 
-  if (e.target.tagName !== 'A') {
-    playAudioHandler(e);
-  }
-});
-
-// Podcasts
-
-function addPodcastHtmlToNode(node, data) {
-  var totalHtml = '';
-  data.forEach(function ({showTitle, rssUrl, streamurl, episodeTitle, url, pubDate}) { 
-    var html = 
-    '<li data-url="' + url +'">' +
-      '<h5 class="station-title">' + showTitle + '</h5>' +
-      '<div class="description">' + episodeTitle + '</div>' +
-      '<div class="last-update">' + formatDateTime(pubDate, false) + '</div>' +
-      '<div class="internet-links">' +      
-          '<a href=' + url + ' target="_blank">Website</a>  | ' + 
-          '<a href=' + rssUrl + ' target="_blank">RSS</a>' +
-      '</div>' +
-    '</li>';
-    totalHtml += html;
-  });
-  $(node).html(totalHtml);
-}
-
-$.ajax({
-  url: servicesURL + '/podcasts',
-  type: 'GET',
-  dataType: 'JSON',
-  success: function (data) {
-    let podcasts = [];
-    Object.keys(data.podcasts).forEach( function(key) { 
-      podcasts.push(data.podcasts[key]);
-    });
-    var podcastColumns = $('.podcast-list');
-    addPodcastHtmlToNode(podcastColumns[0], podcasts.slice(0, 5));
-    addPodcastHtmlToNode(podcastColumns[1], podcasts.slice(5, 10));
-    addPodcastHtmlToNode(podcastColumns[2], podcasts.slice(10, 15));
-
-    $('.podcast-list li').on('click', function (e) { 
-      if (e.target.tagName !== 'A') {
-        playAudioHandler(e);
-      }
-    });
-  }
-});
-
-function playAudioHandler (e) {
-  e.preventDefault();
-
-  if ($(e.target).parent().hasClass('spin')) {
-    return;
-  }
-
-  var clickedAudio = e.currentTarget.getAttribute('data-url');
-
-  if (clickedSameAudio(clickedAudio)) {
-    if (audioPlayer.paused) {
-      audioPlayer.play();
-    } else {
-      audioPlayer.pause();  
-    }
-    return;
-  }
-
-  if ($(e.target).hasClass('radio')) {
-    $('.loading').show();
-  }
-
-  playAudio(clickedAudio);
-}
-
-function playAudio (audioUrl) {
-  loadAudio(audioUrl);
-  audioPlayer.oncanplaythrough = audioPlayer.play();
-}
-
-function loadAudio (audioUrl) {
-  audioSource.attr('src', audioUrl);
-  audioPlayer.pause();
-  audioPlayer.load();
-}
-
-function clickedSameAudio (currentAudioSrc) {
-  return !audioPlayer.ended && (0 < audioPlayer.currentTime)
-  && currentAudioSrc === audioSource.attr('src');
-}
-
-window.addEventListener('keydown', onKeyDown, false);
-
-function onKeyDown (e) {
-  switch (e.keyCode) {
-    case 32: //spacebar                    
-
-      if ($('textarea, input').is(':focus')) {
-        return;
-      }
-
-      e.preventDefault();
-      if (audioPlayer.paused) {            
-          audioPlayer.play();
-      } else {
-          audioPlayer.pause();
-      } 
-      break;
-    case 37: //back arrow
-      audioPlayer.currentTime -= 15
-      break;
-    case 39: //forward arrow
-      audioPlayer.currentTime += 15
-      break;
-    case 27: //esc key
-      $('.banner').hide();
-      break;
-  }
-  return false;
+  return now.toString();
 }
 
 $.ajax({
@@ -177,42 +46,92 @@ function refreshNewsFor (news) {
   })
 }
 
-function formatDateTime (rawDateTimeString, includeTimeFlag) {
-  function getFormattedTime (fourDigitTime) {
-      var hours24 = parseInt(fourDigitTime.substring(0, 2), 10);
-      var hours = ((hours24 + 11) % 12) + 1;
-      var amPm = hours24 > 11 ? 'pm' : 'am';
-      var minutes = fourDigitTime.substring(3, 5);
-
-      return hours + ':' + minutes + amPm;
+$('.news-ul li').on('click', function (e) { 
+  if (e.target.tagName !== 'A') {
+    playAudioHandler(e);
   }
+});
 
-  var dateTimeTokens = new Date(rawDateTimeString).toString().split(' ');
-  var formattedDateTime = dateTimeTokens[1] + " " + dateTimeTokens[2];
 
-  if (includeTimeFlag === false) {
-    return formattedDateTime;
-  }
-
-  formattedDateTime = formattedDateTime +   
-    ", " + getFormattedTime(dateTimeTokens[4]) + 
-    " " + dateTimeTokens[6].replace(/[{()}]/g, '');
-
-  return formattedDateTime;
-};  
-
-function getBBCHeadlinesLastUpdate () {
-  var 
-    minsToChangeTo = 35,
-    now = new Date(),
-    nowMins = now.getMinutes();
-
-  now.setMinutes(minsToChangeTo);
-  var changeToLastHour = (nowMins - minsToChangeTo < 0);
-  if (changeToLastHour) { now.setHours(now.getHours()-1); }
-
-  return now.toString();
+// INTERNET RADIO
+function addHtmlToNode(node, data) {
+  var totalHtml = '';
+  data.forEach(function (el) { 
+    var html = 
+    '<li data-url="' + el.streamurl +'">' +
+      '<h5 class="station-title">' + el.title + '</h5>' +
+      '<div class="description">' + el.description + '</div>' +
+      '<div class="internet-links">' +      
+          '<a href=' + el.url + ' target="_blank">Website</a> | ' + 
+          '<a href=' + el.donateurl + ' target="_blank">Donate</a>' +
+      '</div>' +
+    '</li>';
+    totalHtml += html;
+  });
+  $(node).html(totalHtml);
 }
+
+var internetRadioColumns = $('.internet-ul');
+addHtmlToNode(internetRadioColumns[0], streams.slice(0, 5));
+addHtmlToNode(internetRadioColumns[1], streams.slice(5, 10));
+addHtmlToNode(internetRadioColumns[2], streams.slice(10, 15));
+
+$('.internet-ul li').on('click', function (e) { 
+  if (e.target.tagName !== 'A') {
+    playAudioHandler(e);
+  }
+});
+
+// PODCASTS
+
+function addPodcastHtmlToNode(node, data) {
+  var totalHtml = '';
+  data.forEach(function (el) { 
+    var html = 
+    '<li data-url="' + el.url +'">' +
+      '<h5 class="station-title">' + el.showTitle + '</h5>' +
+      '<div class="description">' + el.episodeTitle + '</div>' +
+      '<div class="last-update">' + formatDateTime(el.pubDate, false) + '</div>' +
+      '<div class="internet-links">' +      
+          '<a href=' + el.url + ' target="_blank">Website</a>  | ' + 
+          '<a href=' + el.rssUrl + ' target="_blank">RSS</a>' +
+      '</div>' +
+    '</li>';
+    totalHtml += html;
+  });
+  $(node).html(totalHtml);
+}
+
+$.ajax({
+  url: servicesURL + '/podcasts',
+  type: 'GET',
+  dataType: 'JSON',
+  success: function (data) {
+    var podcasts = [];
+    Object.keys(data.podcasts).forEach(function (key) { 
+      podcasts.push(data.podcasts[key]);
+    });
+    var podcastColumns = $('.podcast-list');
+    addPodcastHtmlToNode(podcastColumns[0], podcasts.slice(0, 5));
+    addPodcastHtmlToNode(podcastColumns[1], podcasts.slice(5, 10));
+    addPodcastHtmlToNode(podcastColumns[2], podcasts.slice(10, 15));
+
+    $('.podcast-list li').on('click', function (e) { 
+      if (e.target.tagName !== 'A') {
+        playAudioHandler(e);
+      }
+    });
+  }
+});
+
+window.onload = function () {
+  var podToAutoplay = parseInt(window.location.search.replace('?pod=', ''));
+  if (!Number.isNaN(podToAutoplay)) {
+    playAudio($('.listen').get(podToAutoplay).href)
+  }
+};
+
+// REQUESTS
 
 $('.submit').on('click', submitRequest);
 
@@ -270,14 +189,7 @@ function mapErrorCodesToMessages (codes) {
   return messages.reduce(function(a, b) { return a + '</br>' + b; });
 }
 
-function areCookiesEnabled () {
-  var cookieEnabled = (navigator.cookieEnabled) ? true : false;
-  if (typeof navigator.cookieEnabled == "undefined" && !cookieEnabled) { 
-      document.cookie = 'testcookie';
-      cookieEnabled = (document.cookie.indexOf("testcookie") != -1) ? true : false;
-  }
-  return cookieEnabled;
-}
+// ABOUT
 
 function getVisitorCount () {
   if (areCookiesEnabled()) {
@@ -295,12 +207,117 @@ function getVisitorCount () {
   }
 }
 
+function areCookiesEnabled () {
+  var cookieEnabled = (navigator.cookieEnabled) ? true : false;
+  if (typeof navigator.cookieEnabled == "undefined" && !cookieEnabled) { 
+      document.cookie = 'testcookie';
+      cookieEnabled = (document.cookie.indexOf("testcookie") != -1) ? true : false;
+  }
+  return cookieEnabled;
+}
+
 getVisitorCount();
 setInterval(getVisitorCount, 60000); // 1 minute
 
-window.onload = function () {
-  var podToAutoplay = parseInt(window.location.search.replace('?pod=', ''));
-  if (!Number.isNaN(podToAutoplay)) {
-    playAudio($('.listen').get(podToAutoplay).href)
+// GENERAL
+
+// "loading" messaging
+setInterval(function() { $('.loading .text').toggleClass('black') }, 400); // 0.5 second     
+audioPlayer.oncanplay = function () {
+  $('.loading').hide();
+}
+
+function playAudioHandler (e) {
+  e.preventDefault();
+
+  if ($(e.target).parent().hasClass('spin')) {
+    return;
   }
-};
+
+  var clickedAudio = e.currentTarget.getAttribute('data-url');
+
+  if (clickedSameAudio(clickedAudio)) {
+    if (audioPlayer.paused) {
+      audioPlayer.play();
+    } else {
+      audioPlayer.pause();  
+    }
+    return;
+  }
+
+  if ($(e.target).hasClass('radio')) {
+    $('.loading').show();
+  }
+
+  playAudio(clickedAudio);
+}
+
+function playAudio (audioUrl) {
+  loadAudio(audioUrl);
+  audioPlayer.oncanplaythrough = audioPlayer.play();
+}
+
+function loadAudio (audioUrl) {
+  audioSource.attr('src', audioUrl);
+  audioPlayer.pause();
+  audioPlayer.load();
+}
+
+function clickedSameAudio (currentAudioSrc) {
+  return !audioPlayer.ended && (0 < audioPlayer.currentTime)
+  && currentAudioSrc === audioSource.attr('src');
+}
+
+window.addEventListener('keydown', onKeyDown, false);
+
+function onKeyDown (e) {
+  switch (e.keyCode) {
+    case 32: // spacebar                    
+
+      if ($('textarea, input').is(':focus')) {
+        return;
+      }
+
+      e.preventDefault();
+      if (audioPlayer.paused) {            
+          audioPlayer.play();
+      } else {
+          audioPlayer.pause();
+      } 
+      break;
+    case 37: // back arrow
+      audioPlayer.currentTime -= 15
+      break;
+    case 39: // forward arrow
+      audioPlayer.currentTime += 15
+      break;
+    case 27: // esc key
+      $('.banner').hide();
+      break;
+  }
+  return false;
+}
+
+function formatDateTime (rawDateTimeString, includeTimeFlag) {
+  function getFormattedTime (fourDigitTime) {
+      var hours24 = parseInt(fourDigitTime.substring(0, 2), 10);
+      var hours = ((hours24 + 11) % 12) + 1;
+      var amPm = hours24 > 11 ? 'pm' : 'am';
+      var minutes = fourDigitTime.substring(3, 5);
+
+      return hours + ':' + minutes + amPm;
+  }
+
+  var dateTimeTokens = new Date(rawDateTimeString).toString().split(' ');
+  var formattedDateTime = dateTimeTokens[1] + " " + dateTimeTokens[2];
+
+  if (includeTimeFlag === false) {
+    return formattedDateTime;
+  }
+
+  formattedDateTime = formattedDateTime +   
+    ", " + getFormattedTime(dateTimeTokens[4]) + 
+    " " + dateTimeTokens[6].replace(/[{()}]/g, '');
+
+  return formattedDateTime;
+};  
